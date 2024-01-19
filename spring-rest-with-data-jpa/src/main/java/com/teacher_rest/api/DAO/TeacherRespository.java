aac3b329f24abc1a4f1342823d388cd211538d66
Yasir Ali <yasirali.bscssef20@iba-suk.edu.pk>
2024-01-19 19:09:42 +0500

    spring-data-rest added to the project

--- spring-rest-with-data-jpa/src/main/java/com/teacher_rest/api/DAO/TeacherRespository.java
+++ spring-rest-with-data-jpa/src/main/java/com/teacher_rest/api/DAO/TeacherRespository.java
@@ -2,7 +2,9 @@ package com.teacher_rest.api.DAO;
 
 import com.teacher_rest.api.Entity.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface TeacherRespository extends JpaRepository<Teacher,Integer> {
 
 }
