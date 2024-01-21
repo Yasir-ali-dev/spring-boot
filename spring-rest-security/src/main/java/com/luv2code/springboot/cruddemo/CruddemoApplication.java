c17f8057393c073957e5ee3ba85091883ecdd479
Yasir Ali <yasirali.bscssef20@iba-suk.edu.pk>
2024-01-21 18:16:53 +0500

    rest security added to the employee api

--- spring-rest-security/src/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java
+++ spring-rest-security/src/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java
@@ -1,13 +1,13 @@
-package com.employee.springrestdata;
+package com.luv2code.springboot.cruddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringRestDataApplication {
+public class CruddemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringRestDataApplication.class, args);
+		SpringApplication.run(CruddemoApplication.class, args);
 	}
 
 }
