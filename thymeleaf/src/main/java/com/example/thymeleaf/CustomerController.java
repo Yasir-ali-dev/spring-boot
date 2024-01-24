468a01289c51132e62d28376086f80f1aba5fbd9
Yasir Ali <yasirali.bscssef20@iba-suk.edu.pk>
2024-01-24 20:33:23 +0500

    created form with thymeleaf and spring mvc

--- thymeleaf/src/main/java/com/example/thymeleaf/CustomerController.java
+++ thymeleaf/src/main/java/com/example/thymeleaf/CustomerController.java
@@ -1,8 +1,12 @@
 package com.example.thymeleaf;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
@@ -11,4 +15,14 @@ public class CustomerController {
         model.addAttribute("customer",new Customer());
         return "customer-form";
     }
+
+   @PostMapping("/processForm")
+   String processTheForm (@Valid @ModelAttribute("customer") Customer customer , BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "customer-form";
+        }
+        return"customer-confirmation";
+   }
+
 }
+
